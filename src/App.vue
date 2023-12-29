b8771a5ae6c75238814551d5c00868f108347bc7
kbuhantsev <k.buhantsev@gmail.com>
2023-12-29 12:54:56 +0200

    components

--- src/App.vue
+++ src/App.vue
@@ -10,61 +10,7 @@
       </svg>
     </div> -->
     <div class="container">
-      <section>
-        <div class="flex">
-          <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700"> Тикер </label>
-            <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                v-model="ticker"
-                v-on:keydown.enter="add(undefined)"
-                @input="handleTickerInput"
-                type="text"
-                name="wallet"
-                id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                placeholder="Например DOGE"
-              />
-            </div>
-            <div
-              v-for="coin in helpTickers"
-              :key="coin.Id"
-              class="flex bg-white p-1 rounded-md shadow-md flex-wrap"
-            >
-              <span
-                @:click="handleHelpTickerClick(coin)"
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                {{ coin.Symbol }}
-              </span>
-            </div>
-            <div :class="{ invisible: !helpTickersError }" class="text-sm text-red-600">
-              Такой тикер уже добавлен
-            </div>
-          </div>
-        </div>
-        <button
-          @click="add()"
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
-          <!-- Heroicon name: solid/mail -->
-          <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-          >
-            <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
-          Добавить
-        </button>
-      </section>
-
+      <add-ticker />
       <template v-if="tickers.length">
         <div class="max-w-xs">
           Фильтр:
@@ -165,9 +111,14 @@
 
 <script>
 import { subscribeToTicker, unsubscribeFromTicker } from './api'
+import AddTicker from './components/AddTicker.vue'
 
 export default {
   name: 'App',
+
+  components: {
+    AddTicker
+  },
 
   data() {
     return {
---
+++ src/components/AddTicker.vue
@@ -0,0 +1,86 @@
+<template>
+  <section>
+    <div class="flex">
+      <div class="max-w-xs">
+        <label for="wallet" class="block text-sm font-medium text-gray-700"> Тикер </label>
+        <div class="mt-1 relative rounded-md shadow-md">
+          <input
+            v-model="ticker"
+            v-on:keydown.enter="add(undefined)"
+            @input="handleTickerInput"
+            type="text"
+            name="wallet"
+            id="wallet"
+            class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            placeholder="Например DOGE"
+          />
+        </div>
+        <div
+          v-for="coin in helpTickers"
+          :key="coin.Id"
+          class="flex bg-white p-1 rounded-md shadow-md flex-wrap"
+        >
+          <span
+            @:click="handleHelpTickerClick(coin)"
+            class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+          >
+            {{ coin.Symbol }}
+          </span>
+        </div>
+        <div :class="{ invisible: !helpTickersError }" class="text-sm text-red-600">
+          Такой тикер уже добавлен
+        </div>
+      </div>
+    </div>
+    <add-button @click="add()" type="button" class="my-4" />
+  </section>
+</template>
+
+<script>
+import AddButton from './AddButton.vue'
+
+export default {
+  components: {
+    AddButton
+  },
+
+  data() {
+    return {
+      ticker: '',
+      coins: [],
+      helpTickers: []
+    }
+  },
+
+  mounted() {
+    const getCoins = async () => {
+      const resp = await fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
+      const result = await resp.json()
+      this.coins = Object.values(result.Data)
+    }
+    getCoins()
+  },
+
+  methods: {
+    add() {
+      const currentTicker = {
+        name: this.ticker,
+        price: '-',
+        intervalId: null
+      }
+
+      const founded = this.coins.find((coin) => {
+        return coin.Symbol.toLowerCase() === currentTicker.name.toLowerCase()
+      })
+      if (!founded) {
+        alert('No such coin!')
+        return
+      }
+
+      this.$emit('add-ticker', this.ticker)
+      this.ticker = ''
+      this.helpTickers = []
+    }
+  }
+}
+</script>
